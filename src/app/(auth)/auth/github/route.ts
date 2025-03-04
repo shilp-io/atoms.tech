adb6adee8cb7b64b50156bfebdcc6716d01ca209
kn9ck <oknackk@gmail.com>
2025-03-04 14:59:33 -0700

    potential fix

--- src/app/(auth)/auth/github/route.ts
+++ src/app/(auth)/auth/github/route.ts
@@ -2,15 +2,17 @@ import { createClient } from '@/lib/supabase/supabaseServer';
 import { redirect } from 'next/navigation';
 
 export async function GET(request: Request) {
+    console.log('GitHub auth route called');
     const requestUrl = new URL(request.url);
     const supabase = await createClient();
 
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'github',
         options: {
-            redirectTo: `${requestUrl.origin}/auth/callback`,
+          redirectTo: `${requestUrl.origin}/auth/callback`,
         },
-    });
+      })
+      
 
     if (error) {
         return redirect('/login?error=Could not authenticate with GitHub');
